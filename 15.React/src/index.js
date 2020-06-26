0f17aa94df385061a3ba06844fd95770540a3490
dushmanexzy <exzy92@gmail.com>
2020-06-26 23:45:19 +0300

    correct index.js. Test continue

--- 15.React/src/index.js
+++ 15.React/src/index.js
@@ -6,26 +6,103 @@ class CommentWidget extends React.Component {
     super();
 
     this.state = {
-      comments: [
-        {author: '', text: 'Вы можете оставить свой комментарий..', date: '', time: ''}
-      ]
+      comments: [],
+      commentText: ''
     };
   }
 
+  addComment() {
+    const comments = this.state.comments;
+    const date = new Date();
+
+    const newDate = [date.getDate(), date.getMonth(), date.getFullYear()].join('-');
+    const newTime = [date.getHours(), date.getMinutes(), date.getSeconds()].join(':');
+    comments.push({
+      author: this.state.newAuthor,
+      text: this.state.newCommentText,
+      date: newDate,
+      time: newTime
+    });
+
+    this.setState({
+      comments,
+      newAuthor: '',
+      newCommentText: '',
+      newDate: '',
+      newTime: ''
+    });
+  }
+
+  removeComment(key) {
+    const comments = this.state.comments.filter((key, i) => { i !== key })
+
+    this.setState({
+      comments
+    })
+  }
+
+
   render() {
     return (
-      <ul>
-        {
-          this.state.comments.map(comment => {
-            return (
-              <li key={comment.date}>
-                {comment.text}
-              </li>
-            )
-          })
-        }
-      </ul>
-    )
+      <div>
+        <ul>
+          {
+            this.state.comments.map((comment, i) => {
+              return (
+                <li key={i}>
+                  <p>`{comment.author}: {comment.text} {comment.date} {comment.time}`</p>
+                  <button
+                    className="remove__btn"
+                    type="button"
+                    onClick={evt => {
+                      evt.preventDefault()
+                      this.removeComment();
+                    }}
+                  >Remove</button>
+                </li>
+              )
+            })
+          }
+        </ul>
+
+        <form>
+          <label>Input name
+            <input
+              type="text"
+              placeholder="Input your name"
+              value={this.state.author}
+              onCange={evt => {
+                this.setState({ newAuthor: evt.target.value })
+              }}
+            />
+          </label>
+
+          <label>Input comment
+            <input
+              type="text"
+              placeholder="Input new comment"
+              value={this.state.text}
+              onCange={evt => {
+                this.setState({ newCommentText: evt.target.value })
+              }}
+              onKeyUp={evt => {
+                if (evt.keyCode === 13) {
+                  this.addComment();
+                }
+              }}
+            />
+          </label>
+          <button
+            className="submit__btn"
+            type="submit"
+            onClick={evt => {
+              evt.preventDefault()
+              this.addComment();
+            }}
+          >Add</button>
+        </form>
+      </div>
+    );
   }
 }
 
